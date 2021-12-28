fda77bb2d646489eaf760d58b06f1e4de6627303
davidaj61 <davood.a.j@gmail.com>
2021-12-28 12:30:00 +0330

    4001007-1230

--- HRP.Core/Interfaces/IGynecologist.cs
+++ HRP.Core/Interfaces/IGynecologist.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using HRP.DAL.Entities;
 
 namespace HRP.Core.Interfaces
 {
-    interface IGynecologist:IDisposable
+    interface IGynecologist : IDisposable
     {
+        IList<Gynecologist> GetAll();
+
+        bool Add(Gynecologist gynecologist);
+
+
+        bool Update(Gynecologist gynecologist);
+
 
 
     }
---
+++ HRP.Core/Services/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRP.Core.Services
+{
+    class User
+    {
+    }
+}
