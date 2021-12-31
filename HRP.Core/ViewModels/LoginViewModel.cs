a74319547170d4f34244a5dd9d5cffef9d1f40fc
davidaj61 <davood.a.j@gmail.com>
2021-12-31 16:06:01 +0330

    4001010-1605

---
+++ HRP.Core/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HRP.Core.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "نام کاربری وارد نشده است!؟")]
+        [MaxLength(10,ErrorMessage ="نام کاربری معتبر نمی باشد.")]
+        [RegularExpression(@"/\d{10}",ErrorMessage = "نام کاربری معتبر نمی باشد.")]
+        public string UserName { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="کلمه عبور وارد نشده است!؟")]
+        [DataType(nameof(Password))]
+        public string Password { get; set; }
+    }
+}
