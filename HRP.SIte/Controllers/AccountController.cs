d9199721935d20cb90dd2772bac729535836c2a6
davidaj61 <davood.a.j@gmail.com>
2022-01-03 14:31:29 +0330

    4001013-1430

--- HRP.SIte/Controllers/AccountController.cs
+++ HRP.SIte/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HRP.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,15 @@ namespace HRP.SIte.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signinManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signinManager)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+        }
+        
         [HttpGet]
         public IActionResult Login()
         {
