914447171e8738bca2e2516dc48a464616ac548d
davidaj61 <davood.a.j@gmail.com>
2022-01-11 23:07:35 +0330

    4001021-2310

--- HRP.Core/Interfaces/Admin.cs
+++ HRP.Core/Interfaces/Admin.cs
@@ -7,9 +7,33 @@ namespace HRP.Core.Interfaces
 {
     public interface IAdmin
     {
-        List<Person> GetAllUser();
-        void AddUser(Person person,User user);
-        void UpdateUser(Person person, User user);
+        #region Peson
+
+        List<Person> GetAllPerson();
+        void AddPerson(Person person);
+        void UpdatePerson(Person person);
+        void DeletePerson(Person person);
+
+        #endregion
+
+        #region Role
+
+        List<Role> GetAllRoles();
+        void AddRole(Role role);
+        void UpdateRole(Role role);
+        void DeleteRole(Role role);
+
+        #endregion
+
+        #region User
+        List<User> GetAllUsers();
+        void AddUser(User role);
+        void UpdateUsere(User role);
+        void DeleteUser(User role);
+
+
+        #endregion
+
 
     }
 }
